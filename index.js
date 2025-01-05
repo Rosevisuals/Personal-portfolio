8dd2aa08b3e1eb7c2e541aa7b5cd872f00f07d75
Namubiru Rose <rosemichellw@gmail.com>
2025-01-05 19:41:16 +0300

    final edit

--- index.js
+++ index.js
@@ -46,3 +46,13 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active'); // Toggle hamburger animation
     navLinks.classList.toggle('active'); // Show or hide the nav links
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
